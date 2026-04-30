6f8e7228fb3c8fb1da67235b500370c895750ab8
Булат <4bxlia4@gmail.com>
2026-04-30 18:28:40 +0500

    DBMS DONE

--- Functions/SQLQuery2-GetNextLearningDate.sql
+++ Functions/SQLQuery2-GetNextLearningDate.sql
@@ -6,11 +6,16 @@ GO
 ALTER FUNCTION GetNextLearningDate(@group_name NVARCHAR(10), @date DATE) RETURNS DATE
 AS
 BEGIN
-    DECLARE @next_date TINYINT = dbo.GetNextLearningDay(@date, @group_name);
+ --   DECLARE @next_date TINYINT = dbo.GetNextLearningDay(@date, @group_name);
     
-    SET @date = DATEADD(DAY, 1, @date);
-    WHILE DATEPART(WEEKDAY, @date) != @next_date
-		SET @date = DATEADD(DAY, 1, @date);
+ --   SET @date = DATEADD(DAY, 1, @date);
+ --   WHILE DATEPART(WEEKDAY, @date) != @next_date
+	--SET @date = DATEADD(DAY, 1, @date);
+	DECLARE @interval AS INT       = CAST(dbo.GetNextLearningDay(@date, @group_name)AS INT) - DATEPART(WEEKDAY, @date) != @next_date
+	If @interval < 0 SET @interval = @interval+7;
+	DECLARE @next_date AS DATE     = DATEADD(DAY, @interval, @date);
+	IF EXISTS(SELECT holiday FROM DaysOFF WHERE [date]=@next_date)
+		SET @next_date = dbo.GetNextLearningDate(@group_name, @next_date);
 
-    RETURN @date;
+    RETURN @next_date;
 END
--- StoredProcedures/SQLQuery0-CHECK PROCEDURES.sql
+++ StoredProcedures/SQLQuery0-CHECK PROCEDURES.sql
@@ -5,10 +5,10 @@ SET DATEFIRST 1;
 
 --DELETE FROM Schedule;
 
---EXECUTE sp_InsertSchedule N'P_421', N'% MS SQL Server', N'Юыху', N'2026-03-26', N'14:00'
+EXECUTE sp_InsertSchedule N'P_421', N'% MS SQL Server', N'Юыху', N'2026-03-26', N'14:00'
 --EXECUTE sp_InsertSchedule N'PD_321', N'% MS SQL Server', N'Юыху', N'2026-03-26', N'14:00', N'2,4,6';
---EXECUTE sp_SelectSchedule;
+EXECUTE sp_SelectSchedule;
 
-EXEC sp_SetAllHolidaysFor 2026;
-SELECT * FROM DaysOFF 
-JOIN Holidays ON (holiday=holiday_id);
+--EXEC sp_SetAllHolidaysFor 2026;
+--SELECT * FROM DaysOFF 
+--JOIN Holidays ON (holiday=holiday_id);
